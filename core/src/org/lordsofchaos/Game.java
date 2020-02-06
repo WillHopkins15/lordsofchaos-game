f569634203f00673c254156b9aaaf55b94789c8d
bcg814 <bcg814@student.bham.ac.uk>
2020-02-06 17:56:34 +0000

    Isometric Tiles

--- core/src/org/lordsofchaos/Game.java
+++ core/src/org/lordsofchaos/Game.java
@@ -3,25 +3,35 @@ package org.lordsofchaos;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
 
 public class Game extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
+	OrthographicCamera camera;
+	IsometricTiledMapRenderer renderer;
+
+	final int width = 1280;
+	final int height = 720;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera(width, height);
+		camera.position.set(width / 2, height / 2, 10);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(camera.combined);
+		camera.update();
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
