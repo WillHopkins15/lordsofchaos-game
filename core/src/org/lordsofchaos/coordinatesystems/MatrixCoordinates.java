7697e6764d1c091d07ee5d329df246526dab710e
TerrifiedOrc <44375524+TerrifiedOrc@users.noreply.github.com>
2020-02-17 11:11:00 +0000

    Removed matrix flip, added value clamping in matrix coordinates

--- core/src/org/lordsofchaos/coordinatesystems/MatrixCoordinates.java
+++ core/src/org/lordsofchaos/coordinatesystems/MatrixCoordinates.java
@@ -15,7 +15,24 @@ public class MatrixCoordinates extends Coordinates
 		int sf = GameController.getScaleFactor();
 		int y = (Math.round(rwc.getY() / sf));
 		int x =  (Math.round(rwc.getX() / sf));
-		setY(y);
-		setX(x);
+		setY(clamp(y, 0, GameController.getMap().length));
+		setX(clamp(x, 0, GameController.getMap()[0].length));
+	}
+
+	private static int clamp(int value, int min, int max)
+	{
+		int newVal = value;
+		if (newVal < min)
+		{
+			return min;
+		}
+		else if (newVal > max)
+		{
+			return max;
+		}
+		else
+		{
+			return newVal;
+		}
 	}
 }
