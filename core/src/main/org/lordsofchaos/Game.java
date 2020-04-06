d803c79f233442536f8ca5422e4cc0af739bec38
REz <rez20919@yahoo.com>
2020-04-06 15:27:21 +0100

    Fixed alert glitch on local

--- core/src/main/org/lordsofchaos/Game.java
+++ core/src/main/org/lordsofchaos/Game.java
@@ -277,15 +277,23 @@ public class Game extends ApplicationAdapter implements InputProcessor {
     }
     public static void showAlert() {
         if (!alertList.isEmpty()) {
+            int i = 0;
             /*for(int i = 0; i < alertList.size(); i++ ) {
                 System.out.println("Alert Name: " + alertList.get(0).getText() +"DeleteStatus: " + alertList.get(i).getDeleteStatus() + " AlertList Size: " + alertList.size());
             }
             */
-            alertList.get(0).update(Gdx.graphics.getDeltaTime(), batch,currentScreen);
-            if (alertList.get(0).getDeleteStatus()) {
+            while(i < alertList.size()) {
+                if (!(alertList.get(i).getCurrentScreen() == currentScreen || alertList.get(i).getCurrentScreen() == null))
+                    i++;
+                else break;
+            }
+            if(i == alertList.size())
+                return;
+            alertList.get(i).update(Gdx.graphics.getDeltaTime(), batch,currentScreen);
+            if (alertList.get(i).getDeleteStatus()) {
                 //System.out.println("DeletedAlert!!!!");
                 //alertList.get(0).dispose();
-                alertList.remove(0);
+                alertList.remove(i);
             }
 
         }
@@ -440,7 +448,7 @@ public class Game extends ApplicationAdapter implements InputProcessor {
         if(GameController.getDefenderUpgrade() == 3 && doOnceDefender){
             //changedTurn = true;
             doOnceDefender = false;
-            createAlert(10,font,"You reached Tier 3" + '\n' + "  Survive this turn!",Gdx.graphics.getWidth() / 2 - 230, Gdx.graphics.getHeight() - 100,Screen.DEFENDER_SCREEN);
+            createAlert(2,font,"You reached Tier 3" + '\n' + " Survive this turn!",Gdx.graphics.getWidth() / 2 - 230, Gdx.graphics.getHeight() - 100,Screen.DEFENDER_SCREEN);
             //showAlert(2,"Survive One More Turn!",Gdx.graphics.getWidth() / 2 - 300, Gdx.graphics.getHeight() - 200);
 
         }
@@ -475,7 +483,7 @@ public class Game extends ApplicationAdapter implements InputProcessor {
         if(GameController.getDefenderUpgrade() == 3 && doOnceAttacker && player == 1){
             //changedTurn = true;
             doOnceAttacker = false;
-            createAlert(10,font,"  You reached Tier 3" + '\n' + "Destroy the castle now!",Gdx.graphics.getWidth() / 2 - 250, Gdx.graphics.getHeight() - 100,Screen.ATTACKER_SCREEN);
+            createAlert(2,font,"Defender reached Tier 3" + '\n' + " Destroy the castle now!",Gdx.graphics.getWidth() / 2 - 300, Gdx.graphics.getHeight() - 100,Screen.ATTACKER_SCREEN);
             //showAlert(2,"Survive One More Turn!",Gdx.graphics.getWidth() / 2 - 300, Gdx.graphics.getHeight() - 200);
 
         }
