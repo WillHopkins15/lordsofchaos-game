0d437ad469b3a849a2b5988e5e2a6a3885d67fa1
REz <rez20919@yahoo.com>
2020-04-08 15:27:39 +0100

    Hover UI

--- core/src/main/org/lordsofchaos/graphics/buttons/UnitUpgradeButton.java
+++ core/src/main/org/lordsofchaos/graphics/buttons/UnitUpgradeButton.java
@@ -1,19 +1,21 @@
-package org.lordsofchaos.graphics;
+package org.lordsofchaos.graphics.buttons;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import org.lordsofchaos.GameController;
+import org.lordsofchaos.graphics.Screen;
 
-public class UnitUpgradeSprite {
+public class UnitUpgradeSprite extends HoverButton {
     private String path;
     private int x;
     private int y;
     private Texture texture;
     private Sprite sprite;
     private int currentUpgrade;
-    public  UnitUpgradeSprite(String path,int x,int y){
+    public UnitUpgradeSprite(String path, float buttonX1, float buttonY1, Screen screenLocation) {
+        super(path, buttonX1, buttonY1, screenLocation);
         this.path = path;
         this.x = x;
         this.y = y;
@@ -29,12 +31,17 @@ public class UnitUpgradeSprite {
             createSprite(path);
         }
         //System.out.println(currentUpgrade);
-        sprite.draw(batch);
+        //sprite.draw(batch);
     }
     private void createSprite(String path){
         texture.dispose();
         texture = new Texture(Gdx.files.internal(path + currentUpgrade + ".png"));
         sprite = new Sprite(texture);
         sprite.setPosition(x,y);
+    }
+
+    @Override
+    public void update(int x, int y, SpriteBatch batch) {
+
     }
 }
