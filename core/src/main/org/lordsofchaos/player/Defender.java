7d62fbe23600c40bd99cf5f80059d5eabbb1df36
REz <rez20919@yahoo.com>
2020-04-11 16:21:50 +0100

    Starting coins fixed

--- core/src/main/org/lordsofchaos/player/Defender.java
+++ core/src/main/org/lordsofchaos/player/Defender.java
@@ -19,7 +19,7 @@ public class Defender extends Player
     
     public Defender(String Name) {
         super(Name);
-        setCurrentMoney(1000);
+        setCurrentMoney(100);
         setHealth(100);
         //System.out.println("TEST1");
         /*
